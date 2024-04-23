0e82a75c67bf2314f2b85433133450ca8ce87f89
Denden2536 <denzelhutchinson@gmail.com>
2024-04-23 16:09:48 -0400

    tips.JS

---
+++ demos/stratchpad.js
@@ -0,0 +1,9 @@
+var fisrtName = "Denzel";
+var lastName = "Hutchinson";
+var city = "Brooklyn";
+var state = "New York";
+
+console.log(fisrtName);
+console.log(lastName);
+console.log(city);
+console.log(state);
