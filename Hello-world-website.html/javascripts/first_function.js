390692562614e12fa4471b8a28f26b0a627f9e29
Denden2536 <denzelhutchinson@gmail.com>
2024-04-26 15:54:58 -0400

    greet button

---
+++ Hello-world-website.html/javascripts/first_function.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function greet(){
+    let message = "Hello World!";
+console.log(message);
+}
+
+function brushedTeeth(){
+console.log("Get Up");
+console.log("Walk to the bathroom");
+console.log("Open bathroom door")
+console.log("brushed")
+}
